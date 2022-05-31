3ce2da1fd5a2efff6f5a61069b58f77687f6e77d
code <codecode60.protonmail.com>
2022-05-31 16:13:41 +0930

    CRUD up, vars passing

--- storeFront/src/App.tsx
+++ storeFront/src/App.tsx
@@ -2,6 +2,9 @@
 import { useMutation, useQuery } from "urql";
 import { Counter } from "./features/counter/Counter";
 import { Logger } from "./features/login/Login";
+import { RootState } from "./store";
+import { useSelector, useDispatch } from "react-redux";
+import {setItem, setPrice} from "./features/itemSlice";
 import "./App.css";
 
 const getItemsQuery = `
@@ -27,31 +30,107 @@ const PostItem = `
   }
 `;
 
+const DeleteItem = `
+mutation ($id: ID!) {
+  deleteItem(id: $id)
+}
+`;
+
+const UpdateItem = `
+  mutation ($id: ID!, $item: String!, $price: Int!) {
+    updateItem(
+      id: $id,
+      inventory_item: 
+        {
+          item: $item,
+          price: $price
+        }
+    )
+    {
+      id
+      item
+      price
+    }
+  }
+  `;
+
 function App() {
   const [result, refreshQuery] = useQuery({
     query: getItemsQuery,
   });
   const { data, fetching, error } = result;
 
-    const [postItemResult, postItem] = useMutation(PostItem);
+  const [postItemResult, postItem] = useMutation(PostItem);
+  const [deleteItemResult, deleteItem] = useMutation(DeleteItem);
+  const [updateItemResult, updateItem] = useMutation(UpdateItem);
 
-    const submit = (item: string, price: number) => {
-      const variables = { item, price };
-      postItem(variables).then((result) => {
-        console.log(JSON.stringify(result.data));
-        if (result.error) {
-          console.error("Something has gone wrong", result.error);
-        }
-      });
-    };
+  const itemName = useSelector((state:RootState) => state.item.item)
+  const itemPrice = useSelector((state:RootState) => state.item.price)
+  const dispatch = useDispatch();
+
+  const submit = (item: string, price: number) => {
+    const variables = { item, price };
+    postItem(variables).then((result) => {
+      console.log(JSON.stringify(result.data));
+      if (result.error) {
+        console.error("Something has gone wrong", result.error);
+      }
+    });
+  };
+  const submitDelete = (id: string) => {
+    // const variables = { "id": id};
+    deleteItem({ id }).then((result) => {
+      console.log(JSON.stringify(result.data));
+      if (result.error) {
+        console.error("Something has gone wrong", result.error);
+      }
+    });
+  };
+  const submitUpdate = (id: string, item: string, price: number) => {
+    const variables = { id, item, price };
+    updateItem(variables).then((result) => {
+      console.log(JSON.stringify(result.data));
+      if (result.error) {
+        console.error("Something has gone wrong", result.error);
+      }
+    });
+  };
 
   if (fetching) return <p>Loading...</p>;
   if (error) return <p>Aw, hell no: {error.message}</p>;
 
+  
+  
+  const handleForm = (e: any) => {
+    e.preventDefault();
+    return;
+  }
+
+  
+
   return (
     <div>
       Hello World! {JSON.stringify(data.getAllItems)}
-      <button onClick={()=>submit("item3000", 500)}>Post item 3000?</button>
+      <button onClick={() => submit("item3000", 500)}>Post item 3000?</button>
+      <button onClick={() => submitDelete("6295a22661184de26beeade2")}>
+        Delete item 3000?
+      </button>
+      <button
+        onClick={() =>
+          submitUpdate("6295b053b2019bc05879c1f3", "itemUPDATED", 18)
+        }
+      >
+        Update item 3000?
+      </button>
+      <form onSubmit={(e)=>handleForm(e)}>
+        <label htmlFor="itemName">Enter an Item Name:</label>
+        <input id="itemName" type="text" onChange={(e)=>dispatch(setItem(e.target.value))}/>
+        <label htmlFor="itemPrice">Enter an Item Price:</label>
+        <input id="itemPrice" type="text" onChange={(e)=>dispatch(setPrice(parseInt(e.target.value)))}/>
+        <button onClick={()=>submit(itemName, itemPrice)}>Post a new item</button>
+      </form>
+      <span>Item Name: {itemName}</span>
+      <span>Item Price: {itemPrice}</span>
       <Counter />
       <Logger />
     </div>
--- storeFront/src/store.ts
+++ storeFront/src/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/counterSlice";
 import loginReducer from "./features/login/loginSlice";
+import itemReducer from "./features/itemSlice";
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
-        login: loginReducer
+        login: loginReducer,
+        item: itemReducer
     },
 })
 
